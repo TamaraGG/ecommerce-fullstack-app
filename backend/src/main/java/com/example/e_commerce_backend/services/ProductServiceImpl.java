cc287e8406aea1a539529e6117d8234c7ea35ff3
ILOveMyasko <brutalkin_v@mail.ru>
2025-12-12 09:59:39 +0300

    added functions to keep user rating up to date

--- backend/src/main/java/com/example/e_commerce_backend/services/ProductServiceImpl.java
+++ backend/src/main/java/com/example/e_commerce_backend/services/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.e_commerce_backend.services;
 
+import com.example.e_commerce_backend.dtos.CategoryStatsDto;
 import com.example.e_commerce_backend.dtos.CreateProductRequestDto;
 import com.example.e_commerce_backend.dtos.ProductDto;
 import com.example.e_commerce_backend.exceptions.ResourceNotFoundException;
@@ -8,17 +9,21 @@ import com.example.e_commerce_backend.models.Product;
 import com.example.e_commerce_backend.repos.ProductRepository;
 import com.example.e_commerce_backend.services.interfaces.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     @Override
     public ProductDto getById(String id) {
@@ -27,8 +32,8 @@ public class ProductServiceImpl implements ProductService {
         return ProductMapper.toProductDto(retrievedProduct);
     }
 
-    @Override
-    public ProductDto save(CreateProductRequestDto request) {
+    @Override @Transactional
+    public ProductDto createProduct(CreateProductRequestDto request) {
         Objects.requireNonNull(request, "Product name must not be null");
         Product newProduct = Product.builder()
                 .name(request.getName())
@@ -40,6 +45,7 @@ public class ProductServiceImpl implements ProductService {
                 .quantity(request.getQuantity())
                 .build();
         Product savedProduct = productRepository.save(newProduct);
+        refreshCategories(); //TODO: not sure this is good
         return ProductMapper.toProductDto(savedProduct);
     }
 
@@ -53,4 +59,34 @@ public class ProductServiceImpl implements ProductService {
         }
         return products.map(ProductMapper::toProductDto);
     }
+
+    @Override
+    public void updateProductRating(String productId, Integer rating) {
+        Product pToUpdate = productRepository.findById(productId)
+                .orElseThrow(()-> new ResourceNotFoundException("Product not found"));
+        int oldReviewCount = pToUpdate.getReviewCount();
+        double newRating = (pToUpdate.getAverageRating()*oldReviewCount + rating)
+                / (oldReviewCount+1);
+        Double newRoundedRating = Math.round(newRating*100.0)/100.0;
+
+        pToUpdate.setAverageRating(newRoundedRating);
+        pToUpdate.setReviewCount(oldReviewCount+1);
+        productRepository.save(pToUpdate);
+    }
+
+    @Override
+    public boolean existsById(String productId) {
+        return productRepository.existsById(productId);
+    }
+
+    @Override  @Cacheable("categories")
+    public List<CategoryStatsDto> findAllCategories() {
+        return productRepository.findAllCategories();
+    }
+
+    @CacheEvict(value = "categories", allEntries = true)
+    public void refreshCategories() {
+    }
+
+
 }
