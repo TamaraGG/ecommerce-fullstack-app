9dc64f6c281730c6e4b4fd262002898068a0b42b
ILOveMyasko <brutalkin_v@mail.ru>
2025-12-12 08:13:25 +0300

    created model for Review

---
+++ backend/src/main/java/com/example/e_commerce_backend/dtos/ReviewDto.java
@@ -0,0 +1,9 @@
+package com.example.e_commerce_backend.dtos;
+
+import lombok.Builder;
+
+import java.time.Instant;
+
+@Builder
+public record ReviewDto(String id, String productId, String userId, String author, Integer rating, String comment, Instant createdAt) {
+}
