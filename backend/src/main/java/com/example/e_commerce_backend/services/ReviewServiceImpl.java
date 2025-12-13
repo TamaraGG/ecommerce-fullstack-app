312e3d1482f28fe03b0a970138159ba7f561ab65
ILOveMyasko <brutalkin_v@mail.ru>
2025-12-13 03:28:02 +0300

    removed unused imports

--- backend/src/main/java/com/example/e_commerce_backend/services/ReviewServiceImpl.java
+++ backend/src/main/java/com/example/e_commerce_backend/services/ReviewServiceImpl.java
@@ -10,14 +10,11 @@ import com.example.e_commerce_backend.repos.ReviewRepository;
 import com.example.e_commerce_backend.services.interfaces.ProductService;
 import com.example.e_commerce_backend.services.interfaces.ReviewService;
 import lombok.AllArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Objects;
 
 @Service
