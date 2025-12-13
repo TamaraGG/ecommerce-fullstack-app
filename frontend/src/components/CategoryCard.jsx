550b3358fde9db6fcdaaca76091f6421881162fc
TamaraGG <goncharova2804t@gmail.com>
2025-12-13 18:58:06 +0300

    added image to category card

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import image from "../assets/default.png";
 
 function CategoryCard({ cat }) {
   const link = `products/?category=${cat.category}`;
+  const catImage = cat.imageBase64 ? cat.imageBase64 : image;
 
   return (
     <>
@@ -9,6 +11,7 @@ function CategoryCard({ cat }) {
         <Link to={link}>
           <h4>{cat.category}</h4>
         </Link>
+        <img src={catImage} alt="" width="75px" />
         <p>Number of products: {cat.count}</p>
       </div>
     </>
