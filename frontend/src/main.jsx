df1c7ef871b35dc4558cdda252ee6e5c8a6edc97
TamaraGG <goncharova2804t@gmail.com>
2025-12-13 05:26:13 +0300

    frontend second commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import ProductPage from "./pages/ProductPage.jsx";
 import MainPage from "./pages/MainPage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CategoryProductsList from "./pages/CategoryProductsList.jsx";
 import MainLayout from "./components/layouts/MainLayout.jsx";
 import CartProvider from "./context/CartProvider.jsx";
 import CartPage from "./pages/CartPage.jsx";
+import SortedPaginatedProductsListPage from "./pages/SortedPaginatedProductsListPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,8 +18,14 @@ const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path: "category/:catName",
-        element: <CategoryProductsList />,
+        path: "products/",
+        element: (
+          <SortedPaginatedProductsListPage
+            url={""}
+            filterParam={"category"}
+            emptyMessage={"no products"}
+          />
+        ),
       },
       {
         path: "products/:id",
@@ -28,6 +34,16 @@ const router = createBrowserRouter([
       {
         path: "cart",
         element: <CartPage />,
+      },
+      {
+        path: "search",
+        element: (
+          <SortedPaginatedProductsListPage
+            url={"/search"}
+            filterParam={"pattern"}
+            emptyMessage={"no products"}
+          />
+        ),
       },
       // 5. Маршрут 404 (Необязательно, но полезно):
       // Добавьте этот маршрут в MainLayout, если он должен иметь шапку/подвал.
