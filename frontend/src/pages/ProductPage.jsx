f51593351e484cdd31d935b27a4281366f4bfb37
TamaraGG <goncharova2804t@gmail.com>
2025-12-13 18:42:46 +0300

    added ProductPage with product card and reviews list (without design)

---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import ProductCard from "../components/ProductCard";
+import { getProducts } from "../services/ProductService";
+import ReviewsList from "../components/reviews/ReviewList";
+
+function ProductPage() {
+  const { id } = useParams();
+
+  const fetchData = () => getProducts(id);
+
+  const { data: product, isLoading, error } = useFetch(fetchData, [id]);
+
+  return (
+    <div>
+      <div>
+        {isLoading && <p>Loading product details...</p>}
+        {error && <p>ERROR: {error?.message || "Error while loading."}</p>}
+
+        {!isLoading && !error && product && <ProductCard product={product} />}
+
+        {!isLoading && !error && !product && <p>Product not found.</p>}
+      </div>
+      <div>
+        <ReviewsList productId={id} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
